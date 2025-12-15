6adcfc9e94a9cc4528a4406b3b987f8b7c3841d3
lcx.samuel0226-cmd <lcx.samuel0226@gmail.com>
2025-12-15 23:57:25 +0800

    Initial Commit

---
+++ DS1HW5_16__11327132__11327141/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DS1HW5_16__11327132__11327141
+//
+//  Created by 李晨希 on 2025/12/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
